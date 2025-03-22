71ea79a2dddbd660117691a0f92f024534d8e889
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-03-22 13:22:31 +0100

    feat: create and render MovieCard copmonent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Search from "./components/search";
+import Spinner from "./components/spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -59,16 +61,16 @@ const App = () => {
 				</header>
 
 				<section className="all-movies">
-					<h2>All movies</h2>
+					<h2 className="mt-[40px]">All movies</h2>
 
 					{isLoading ? (
-						<p className="text-white">Loading ...</p>
+						<Spinner />
 					) : errorMessage ? (
 						<p className="text-red-500">{errorMessage}</p>
 					) : (
 						<ul>
 							{movieList.map((movie) => (
-								<p className="text-white">{movie.title}</p>
+								<MovieCard key={movie.id} movie={movie} />
 							))}
 						</ul>
 					)}
